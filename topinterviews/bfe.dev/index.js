fcc56897d23affe41ee13760a5811f2badd817c7
Anudeep Dasari <user@Users-MacBook-Air.local>
2023-05-08 08:55:59 -0700

    updated content

--- topinterviews/bfe.dev/index.js
+++ topinterviews/bfe.dev/index.js
@@ -1956,3 +1956,21 @@ useEffect(() => {
     abortController.abort();
   };
 }, [id]);
+
+// Route level Code-Splitting
+import React, { Suspense, lazy } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+const Home = lazy(() => import('./routes/Home'));
+const About = lazy(() => import('./routes/About'));
+
+const App = () => (
+  <Router>
+    <Suspense fallback={<div>Loading...</div>}>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </Suspense>
+  </Router>
+);
