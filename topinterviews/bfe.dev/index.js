3aa2274c8071a580c9bccee06ec45cfae70c01ad
Anudeep Dasari <user@Users-MacBook-Air.local>
2023-05-09 16:31:41 -0700

    sysdesign update

--- topinterviews/bfe.dev/index.js
+++ topinterviews/bfe.dev/index.js
@@ -1999,3 +1999,18 @@ document.addEventListener("DOMContentLoaded", function() {
     // Possibly fall back to event handlers here
   }
 });
+
+async function fetchWithTimeout(resource, options = {}) {
+  const { timeout = 8000 } = options;
+  
+  const controller = new AbortController();
+  const id = setTimeout(() => controller.abort(), timeout);
+
+  const response = await fetch(resource, {
+    ...options,
+    signal: controller.signal  
+  });
+  clearTimeout(id);
+
+  return response;
+}
